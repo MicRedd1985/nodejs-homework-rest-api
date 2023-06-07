1fcff07eaee7155b6f6beac1d5a4292b14eca547
MicRedd1985 <micredd1985@gmail.com>
2023-06-07 12:13:44 +0300

    fix

--- middlewares/user.js
+++ middlewares/user.js
@@ -6,10 +6,12 @@ const { SECRET_KEY } = process.env;
 const user = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  
+  try 
+  {
+    if (bearer !== "Bearer") {
     throw new Unauthorized("Not authorized");
   }
-  try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
